966ec86ca92be94d2bd71f6adb44c6e3d4b26fa8
Handsome Dragon <31274965+18070952318@users.noreply.github.com>
2017-12-26 12:16:05 +0800

    messageBoard
    with1400310223庞俊龙

--- messageboard.sql
+++ messageboard.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-12-25 20:53:55
+Date: 2017-12-26 09:39:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,7 @@ CREATE TABLE `message` (
   `CREATETIME` varchar(255) DEFAULT NULL,
   `ISVAL` int(11) DEFAULT NULL,
   PRIMARY KEY (`MESSAGEID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of message
@@ -41,6 +41,9 @@ INSERT INTO `message` VALUES ('10', 'test顶顶顶顶顶顶顶', '游客', '2017
 INSERT INTO `message` VALUES ('12', '灌灌灌灌灌灌', '游客', '2017-12-25 11:53:31', '0');
 INSERT INTO `message` VALUES ('13', '真有趣', '最英俊的恐龙', '2017-12-25 19:12:27', '1');
 INSERT INTO `message` VALUES ('14', '厕所很舒服', '游客', '2017-12-25 19:23:41', '1');
+INSERT INTO `message` VALUES ('15', '水水水水水水', '游客', '2017-12-25 21:52:46', '1');
+INSERT INTO `message` VALUES ('16', '收费收费方式', '游客', '2017-12-25 21:54:06', '0');
+INSERT INTO `message` VALUES ('17', '测试1', '最英俊的恐龙', '2017-12-25 22:57:31', '1');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -54,7 +57,7 @@ CREATE TABLE `user` (
   `ROLL` int(11) DEFAULT NULL,
   `ISREGIS` int(11) DEFAULT NULL,
   PRIMARY KEY (`USERID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
@@ -67,3 +70,4 @@ INSERT INTO `user` VALUES ('5', '公司上市辅导', '的归属感和', '123456
 INSERT INTO `user` VALUES ('6', '啊大哥的撒噶', '广东韶关', '123456', '0', '1');
 INSERT INTO `user` VALUES ('7', '啊是根深蒂固', '额我讨厌我也', '123456', '0', '1');
 INSERT INTO `user` VALUES ('8', 'admin', 'admin', '123456', '1', '1');
+INSERT INTO `user` VALUES ('9', '丑八怪', 'lisi', '123456', '0', '1');
